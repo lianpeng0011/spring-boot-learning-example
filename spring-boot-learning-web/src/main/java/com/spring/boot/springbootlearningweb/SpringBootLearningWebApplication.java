9a1aac03fe1c5e0d235b1ffd8badb7d7eaa360ac
173008335@qq.com <173008335@qq.com>
2018-12-14 22:21:32 +0800

    创建项目

---
+++ spring-boot-learning-web/src/main/java/com/spring/boot/springbootlearningweb/SpringBootLearningWebApplication.java
@@ -0,0 +1,14 @@
+package com.spring.boot.springbootlearningweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLearningWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLearningWebApplication.class, args);
+	}
+
+}
+

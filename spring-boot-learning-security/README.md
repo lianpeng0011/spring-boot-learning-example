76622846fa9beb089325369e690bc1f487b6f98b
lianpeng0011 <173008335@qq.com>
2019-01-21 17:58:26 +0800

    add security

---
+++ spring-boot-learning-security/README.md
@@ -0,0 +1,5 @@
+# spring-boot-learning-security
+
+
+
+
---
+++ spring-boot-learning-security/src/main/java/com/spring/boot/springbootlearningsecurity/SpringBootLearningSecurityApplication.java
@@ -0,0 +1,14 @@
+package com.spring.boot.springbootlearningsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLearningSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLearningSecurityApplication.class, args);
+	}
+
+}
+
